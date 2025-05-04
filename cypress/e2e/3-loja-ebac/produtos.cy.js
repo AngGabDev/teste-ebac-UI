8ace0a15fb8de58aff96104ccc2012d31f62fa08
Angelo <angelogabrielpires09@gmail.com>
2025-05-04 14:15:35 -0300

    Massa de dados em lista

--- cypress/e2e/3-loja-ebac/produtos.cy.js
+++ cypress/e2e/3-loja-ebac/produtos.cy.js
@@ -16,7 +16,7 @@ describe('Funcionalidade: Produtos', () => {
 
     });
 
-    it.only('Deve buscar um produto com sucesso', () => {
+    it('Deve buscar um produto com sucesso', () => {
         
         let produto = 'Apollo Running Short'
 
@@ -27,9 +27,31 @@ describe('Funcionalidade: Produtos', () => {
 
     it('Deve visitar a página de um produto', () => {
         
+        produtosPage.visitarProduto('Apollo Running Short')
+        cy.get('.product_title').should('contain', 'Apollo Running Short')
+
     });
 
     it('Deve adicionar um produto ao carrinho', () => {
+        
+        let qtd = 9
+
+        produtosPage.buscarProduto('Aero Daily Fitness Tee')
+        produtosPage.addCarrinho('M', 'Yellow', qtd)
+        cy.get('.woocommerce-message').should('contain', qtd + ' × “Aero Daily Fitness Tee” foram adicionados no seu carrinho.')
+    });
+
+    it.only('Deve adicionar um produto ao carrinho buscando da massa de dados', () => {
+        
+        cy.fixture('produtos').then(dados => {
+
+    
+            produtosPage.buscarProduto(dados[0].nomeProduto)
+            produtosPage.addCarrinho(dados[0].tamanho, dados[0].cor, dados[0].quantidade)
+            cy.get('.woocommerce-message').should('contain', dados[0].nomeProduto)
+            
+        })
+
         
     });
 });
--- cypress/support/page-objects/produtos.page.js
+++ cypress/support/page-objects/produtos.page.js
@@ -21,15 +21,19 @@ buscarProdutoLista(nomeProduto){
 
 }
 
-visitarProduto(){
+visitarProduto(nomeProduto){
 
-
-
+    //cy.visit(`produtos/${nomeProduto}`)
+    const urlFormatada = nomeProduto.replace(/ /g, '-')
+    cy.visit(`produtos/${urlFormatada}`)
 }
 
-addCarrinho(){
+addCarrinho(tamanho, cor, quantidade){
 
-
+    cy.get('.button-variable-item-' + tamanho).click()
+    cy.get(`.button-variable-item-${cor}`).click()
+    cy.get('.input-text').clear().type(quantidade)
+    cy.get('.single_add_to_cart_button').click()
 
 }
 
